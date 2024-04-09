9a03a1465b2f8c4bfd5002d09ba36740029ce0e6
Ромашко <romashko-bor@mail.ru>
2024-04-09 21:32:38 +0300

    test junit new commit

--- src/test/java/ru/netology/CashBackHackerTest.java
+++ src/test/java/ru/netology/CashBackHackerTest.java
@@ -28,14 +28,5 @@ public class CashBackHackerTest {
         assertEquals(expected, actual);
     }
 
-    @Test
-    public void calculateCashBackDoNotNeeded() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
 
-        int actual = service.remain(amount);
-        int expected = 0;
-
-        assertEquals(expected, actual);
-    }
 }
